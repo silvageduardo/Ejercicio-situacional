7f62da51f3cb8558db43705fd0cd84b61287de8a
Eduardo Silva <silvaeduardo1144@gmail.com>
2021-05-13 23:48:31 -0300

    primera parte

---
+++ js/Ejercicio situacional.js
@@ -0,0 +1,12 @@
+const barNav = document.querySelector("#barNav");
+const nav = `
+<ul class="nav">
+	<li><a href="">Usuario</a>
+		<ul>
+            <li> <a href="Ejercicio situacional.html">Log in</a> </li>
+		</ul>
+	</li>
+</ul>
+`;
+
+barNav.innerHTML = nav;
